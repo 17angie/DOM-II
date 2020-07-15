8268340028ac695dd9ae3480d7abd0ab967b4be0
angelique hill <angeliquehill21@gmail.com>
2020-07-14 22:42:21 -0400

    finished adding events to DOM

--- js/index.js
+++ js/index.js
@@ -10,11 +10,12 @@ busPic.addEventListener('mouseleave', () => {
   busPic.style.transform = 'skew(0deg)';
 })
 
-const adventurePics = document.querySelectorAll('.img-content img');
+const adventurePics = document.querySelectorAll('section');
 
 adventurePics[0].addEventListener('click', () => {
   adventurePics[0].style.transform = 'scale(1.1)';
   adventurePics[0].style.transition = '0.5s';
+
 })
 
 adventurePics[1].addEventListener('click', () => {
@@ -30,6 +31,7 @@ adventurePics[0].addEventListener('dblclick', () => {
 adventurePics[1].addEventListener('dblclick', () => {
   adventurePics[1].style.transform = 'scale(1)';
   adventurePics[1].style.transition = '0.5s';
+
 })
 
 const whereTo = document.querySelector(".content-destination");
@@ -42,6 +44,50 @@ whereTo.addEventListener('mouseout', () => {
   whereTo.style.backgroundColor = 'transparent';
 })
 
-whereTo.addEventListener('select', () => {
-  whereTo.style.color = 'red';
+const webPage = document.querySelector("body");
+
+webPage.addEventListener('keypress', () => {
+  webPage.style.backgroundColor = 'aquamarine';
+})
+
+webPage.addEventListener('keyup', () => {
+  webPage.style.backgroundColor = 'burlywood';
+})
+
+const theP = document.querySelectorAll('p');
+
+theP.forEach(pTag => {
+  pTag.addEventListener('select', () => {
+    webPage.style.color = 'white';
+  })
+})
+
+webPage.addEventListener('select', () => {
+  webPage.style.color = 'white';
+})
+
+const destination = document.querySelectorAll('.destination .btn');
+
+// destination[0].addEventListener('mousedown', () => {
+//   destination[0].style.visibility = 'hidden';
+// })
+
+// destination[1].addEventListener('mousedown', () => {
+//   destination[1].style.visibility = 'hidden';
+// })
+
+// destination[2].addEventListener('mousedown', () => {
+//   destination[2].style.visibility = 'hidden';
+// })
+
+destination.forEach(section => {
+  section.addEventListener('mousedown', () => {
+    section.style.visibility = 'hidden'
+  })
+})
+
+const stopNav = document.querySelector('.nav');
+
+stopNav.addEventListener('click', (event) => {
+  event.preventDefault()
 })
